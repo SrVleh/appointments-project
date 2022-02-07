17d024d4898c3eb9f2fca4880fcaf7f05df24372
TrueVale <srvleh@gmail.com>
2022-02-07 18:26:04 +0100

    Form states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,39 @@
+import { useState, useEffect } from 'react';
+
 const Form = () => {
+
+    const [fName, setFname] = useState('')
+    const [lName, setLname] = useState('')
+    const [email, setEmail] = useState('')
+    const [birthday, setBirth] = useState('')
+    const [symptoms, setSympt] = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        console.log("Sending form...")
+    }
+
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Traker</h2>
             <p className="text-lg mt-5 text-center mb-10">Add and Manage {''} <span className="text-indigo-600 font-bold text-xl">Patients</span></p>
-            <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form 
+                onSubmit={handleSubmit}
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                >
                 <div className="mb-5">
                     <label htmlFor="name" className="block text-gray-700 uppercase font-bold">First Name</label>
                     <input 
                         id="name"
                         type="text"  
                         placeholder="First Name"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={ fName }
+                        onChange={ (e) => setFname(e.target.value) }
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="lname" className="block text-gray-700 uppercase font-bold">Last Name</label>
@@ -18,7 +41,10 @@ const Form = () => {
                         id="lname"
                         type="text"  
                         placeholder="Last Name"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={ lName }
+                        onChange={ (e) => setLname(e.target.value)}
+                        />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
@@ -26,18 +52,27 @@ const Form = () => {
                         id="email"
                         type="email"  
                         placeholder="Email"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={ email }
+                        onChange={ (e) => setEmail(e.target.value)}
+                        />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="birthday" className="block text-gray-700 uppercase font-bold">Birthday</label>
                     <input 
                         id="birthday"
                         type="date"  
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={ birthday }
+                        onChange={ (e) => setBirth(e.target.value)}
+                        />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="symptoms" className="block text-gray-700 uppercase font-bold">Symptoms</label>
-                    <textarea id="symptoms" placeholder="Describe the symptoms" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                    <textarea id="symptoms" placeholder="Describe the symptoms" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    value={ symptoms }
+                    onChange={ (e) => setSympt(e.target.value)}
+                    />
                 </div>
                 <input 
                     type="submit" 
