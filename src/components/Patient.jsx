044c8b769f516684f99ed136abe2fc19530a6e6e
TrueVale <srvleh@gmail.com>
2022-02-08 18:02:50 +0100

    Generate patient data dynamically

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -1,20 +1,23 @@
-const Patient = () => {
+const Patient = ({ patient }) => {
+
+    const { fName, lName, email, birthday, symptoms } = patient
+
   return (
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">First Name: {''}
-            <span className="font-normal normal-case">Mike</span>
+            <span className="font-normal normal-case">{ fName }</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Last Name: {''}
-            <span className="font-normal normal-case">Sick</span>
+            <span className="font-normal normal-case">{ lName }</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
-            <span className="font-normal normal-case">halo@gmail.com</span>
+            <span className="font-normal normal-case">{ email }</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Birthday: {''}
-            <span className="font-normal normal-case">10 Feb 1993</span>
+            <span className="font-normal normal-case">{ birthday }</span>
         </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Symptoms: {''}
-            <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus incidunt, dolores quaerat natus voluptate ut nisi quas earum suscipit alias soluta, voluptatum at voluptatem vel veniam et a sapiente aut.</span>
+            <span className="font-normal normal-case">{ symptoms }</span>
         </p>
     </div>
   );
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,6 +1,6 @@
 import Patient from "./Patient"
 
-const PatientsList = () => {
+const PatientsList = ({ patients }) => {
     return(
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             <h2 className="font-black text-3xl text-center">Patients List</h2>
@@ -8,10 +8,12 @@ const PatientsList = () => {
                 Manage your {''}
                 <span className="text-indigo-600 font-bold text-xl">Patients and Appointments</span>
             </p>
-            <Patient></Patient>
-            <Patient></Patient>
-            <Patient></Patient>
-            <Patient></Patient>
+
+            { patients.map( (patient) => 
+                <Patient
+                    patient={ patient }
+                />
+            )}
         </div>
     )
 }
